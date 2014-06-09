2f301daef267cc67a696626a8cb885e56c0f7514
Damon Xu <damon.xu@calix.com>
2014-06-10 07:02:01 +0800

    Executable done()

---
+++ ExampleProject/src/concurrentExamples/chapter3/executorTaskDone/ExecutableTask.java
@@ -0,0 +1,30 @@
+package concurrentExamples.chapter3.executorTaskDone;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+public class ExecutableTask implements Callable<String> {
+    private String name;
+    
+    public String getName() {
+        return name;
+    }
+    
+    public ExecutableTask(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String call() throws Exception {
+        try{
+            long duration = (long)(Math.random()*10);
+            System.out.printf("%s: Waiting %d seconds for results.\n", this.name,duration);
+            TimeUnit.SECONDS.sleep(duration);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        
+        return "Hello, I'm " + name;
+    }
+    
+}
