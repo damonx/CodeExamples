c949b06a80f076507f36dc301964b68cba287724
Damon Xu <damon.xu@calix.com>
2014-06-12 23:13:58 +0800

    new

---
+++ ExampleProject/src/concurrentExamples/chapter3/rejectExecutionHandler/Main.java
@@ -0,0 +1,28 @@
+package concurrentExamples.chapter3.rejectExecutionHandler;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class Main {
+
+    public static void main(String[] args) {
+       RejectedTaskController controller = new RejectedTaskController();
+       ThreadPoolExecutor executor = (ThreadPoolExecutor)Executors.newCachedThreadPool();
+       executor.setRejectedExecutionHandler(controller);
+       
+       System.out.println("Main: Starting");
+       for (int i=0; i<3; i++){
+           Task task = new Task("Task " + i);
+           executor.submit(task);
+       }
+       System.out.println("Main: Shutting down the Executor.");
+       executor.shutdown();
+       
+       System.out.println("Main: Sending another Task.");
+       Task task = new Task("RejectedTask");
+       executor.submit(task);
+       
+       System.out.println("Main: End");
+     }
+
+}
