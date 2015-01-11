e276a0c6b801af7ee9589a46d334826d652c199f
Damon Xu <akldamonx@icloud.com>
2015-01-11 22:47:00 +0800

    new

---
+++ ExampleProject/src/zenOfDesignPatterns/mediator/Stock.java
@@ -0,0 +1,30 @@
+package zenOfDesignPatterns.mediator;
+
+public class Stock extends AbstractColleage {
+
+	public Stock(AbstractMediator mediator) {
+		super(mediator);
+    }
+	
+	private static int COMPUTER_NUMBER = 100;
+	
+	public void increase(int number){
+		COMPUTER_NUMBER += number;
+		System.out.println("stock number is : " + COMPUTER_NUMBER);
+	}
+	
+	public void decrease(int number){
+		COMPUTER_NUMBER -= number;
+		System.out.println("stock number is : " + COMPUTER_NUMBER);
+	}
+	
+	public int getStockNumber(){
+		return COMPUTER_NUMBER;
+	}
+	
+	public void clearStock(){
+		System.out.println("clear stock nunmber: " + COMPUTER_NUMBER);
+		super.mediator.execute("stock.clear");
+	}
+  
+}
